e8b306b657ae461787b97d39649a1ca38c8809e0
eric-edem <61209061+eric-edem@users.noreply.github.com>
2023-09-30 02:54:20 -0400

    Update featureSelectionResults.py

--- Malicious_Tweet_Detection/zprops/featureSelectionResults.py
+++ Malicious_Tweet_Detection/zprops/featureSelectionResults.py
@@ -1,14 +1,14 @@
 selected_columns = ['account_age', 'Follower Count', 'Following Count', 'User Favourite Count', 'noTweet', 'Retweet Count',
                         'no_hashtag', 'no_mentions', 'Tweet Favourite Count', 'no_digit', 'having_IP_Address', 'having_At_Symbol',
-                        'double_slash_redirecting', 'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'port',
-                        'HTTPS_token', 'Request_URL', 'URL_of_Anchor', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow',
+                        'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'port', 'HTTPS_token', 'Request_URL',
+                        'URL_of_Anchor', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow',
                         'Iframe', 'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'subjectivity', 
                         'polarity', 'sentiment_label', 'TFIDF', 'Tweet_Label']
 
 feature_columns = ['account_age', 'Follower Count', 'Following Count', 'User Favourite Count', 'noTweet', 'Retweet Count',
                         'no_hashtag', 'no_mentions', 'Tweet Favourite Count', 'no_digit', 'having_IP_Address', 'having_At_Symbol',
-                        'double_slash_redirecting', 'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'port',
-                        'HTTPS_token', 'Request_URL', 'URL_of_Anchor', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow',
+                         'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'port', 'HTTPS_token', 'Request_URL',
+                         'URL_of_Anchor', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow',
                         'Iframe', 'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'subjectivity',
                         'polarity', 'sentiment_label', 'TFIDF']
 
