9b357894a1ca669636ecc4179b2fc4b387cd8d6d
eric-edem <61209061+eric-edem@users.noreply.github.com>
2023-09-30 02:25:49 -0400

    Update featureSelectionResults.py

--- Malicious_Tweet_Detection/zprops/featureSelectionResults.py
+++ Malicious_Tweet_Detection/zprops/featureSelectionResults.py
@@ -2,15 +2,15 @@ selected_columns = ['account_age', 'Follower Count', 'Following Count', 'User Fa
                         'no_hashtag', 'no_mentions', 'Tweet Favourite Count', 'no_digit', 'having_IP_Address', 'having_At_Symbol',
                         'double_slash_redirecting', 'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'port',
                         'HTTPS_token', 'Request_URL', 'URL_of_Anchor', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow',
-                        'Iframe', 'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'Links_pointing_to_page',
-                        'subjectivity', 'polarity', 'sentiment_label', 'TFIDF', 'Tweet_Label']
+                        'Iframe', 'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'subjectivity', 
+                        'polarity', 'sentiment_label', 'TFIDF', 'Tweet_Label']
 
 feature_columns = ['account_age', 'Follower Count', 'Following Count', 'User Favourite Count', 'noTweet', 'Retweet Count',
                         'no_hashtag', 'no_mentions', 'Tweet Favourite Count', 'no_digit', 'having_IP_Address', 'having_At_Symbol',
                         'double_slash_redirecting', 'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'port',
                         'HTTPS_token', 'Request_URL', 'URL_of_Anchor', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow',
-                        'Iframe', 'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'Links_pointing_to_page',
-                        'subjectivity', 'polarity', 'sentiment_label', 'TFIDF']
+                        'Iframe', 'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'subjectivity',
+                        'polarity', 'sentiment_label', 'TFIDF']
 
 augmentation_columns = ['account_age', 'Follower Count', 'Following Count',
                       'User Favourite Count', 'no_lists', 'noTweet']
