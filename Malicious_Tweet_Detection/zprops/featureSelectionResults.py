c0582c8afd06532369014975208884549d537b07
eric-edem <61209061+eric-edem@users.noreply.github.com>
2024-03-20 00:40:01 -0400

    feature selection commits

--- Malicious_Tweet_Detection/zprops/featureSelectionResults.py
+++ Malicious_Tweet_Detection/zprops/featureSelectionResults.py
@@ -1,19 +1,17 @@
-selected_columns = ['account_age', 'Follower Count', 'Following Count', 'User Favourite Count', 'noTweet', 'Retweet Count',
-                        'no_hashtag', 'no_mentions', 'Tweet Favourite Count', 'no_digit', 'having_IP_Address', 'having_At_Symbol',
-                        'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'port', 'HTTPS_token', 'Request_URL',
-                        'URL_of_Anchor', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow',
-                        'Iframe', 'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'subjectivity', 
-                        'polarity', 'sentiment_label', 'TFIDF', 'Tweet_Label']
+selected_columns = ['Message Lenght', 'account_age', 'Follower Count','Following Count', 'User Favourite Count','no_lists', 'Retweet Count',
+                'no_hashtag', 'no_mentions','Tweet Favourite Count', 'no_digit', 'no_urls', 'having_IP_Address', 'having_At_Symbol', 
+                'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'Request_URL', 'Google_Index',
+                'URL_of_Anchor', 'Redirect', 'RightClick', 'popUpWidnow', 'Iframe', 'on_mouseover', 'Favicon', 'age_of_domain', 'DNSRecord',
+                'web_traffic', 'Page_Rank', 'subjectivity', 'polarity', 'sentiment_label','TFIDF' 'Tweet_Label']
 
-feature_columns = ['account_age', 'Follower Count', 'Following Count', 'User Favourite Count', 'noTweet', 'Retweet Count',
-                        'no_hashtag', 'no_mentions', 'Tweet Favourite Count', 'no_digit', 'having_IP_Address', 'having_At_Symbol',
-                         'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'port', 'HTTPS_token', 'Request_URL',
-                         'URL_of_Anchor', 'Redirect', 'on_mouseover', 'RightClick', 'popUpWidnow',
-                        'Iframe', 'age_of_domain', 'DNSRecord', 'web_traffic', 'Page_Rank', 'Google_Index', 'subjectivity',
-                        'polarity', 'sentiment_label', 'TFIDF']
+feature_columns = ['Message Lenght', 'account_age', 'Follower Count','Following Count', 'User Favourite Count','no_lists', 'Retweet Count',
+                'no_hashtag', 'no_mentions','Tweet Favourite Count', 'no_digit', 'no_urls', 'having_IP_Address', 'having_At_Symbol', 
+                'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'Request_URL', 'Google_Index',
+                'URL_of_Anchor', 'Redirect', 'RightClick', 'popUpWidnow', 'Iframe', 'on_mouseover', 'Favicon', 'age_of_domain', 'DNSRecord',
+                'web_traffic', 'Page_Rank', 'subjectivity', 'polarity', 'sentiment_label','TFIDF']
 
 augmentation_columns = ['account_age', 'Follower Count', 'Following Count',
-                      'User Favourite Count', 'no_lists', 'noTweet']
+                      'User Favourite Count', 'no_lists']
 
 
 prediction_prop = ['Malicious Tweet !', 'Non-malicious Tweet']
