474e358ce5b98962a455c9e8558307e890f90b1f
eric-edem <61209061+eric-edem@users.noreply.github.com>
2024-03-20 01:37:56 -0400

    minor error - comma fix

--- Malicious_Tweet_Detection/zprops/featureSelectionResults.py
+++ Malicious_Tweet_Detection/zprops/featureSelectionResults.py
@@ -2,7 +2,7 @@ selected_columns = ['Message Lenght', 'account_age', 'Follower Count','Following
                 'no_hashtag', 'no_mentions','Tweet Favourite Count', 'no_digit', 'no_urls', 'having_IP_Address', 'having_At_Symbol', 
                 'Prefix_Suffix', 'having_Sub_Domain',  'Domain_registeration_length', 'Request_URL', 'Google_Index',
                 'URL_of_Anchor', 'Redirect', 'RightClick', 'popUpWidnow', 'Iframe', 'on_mouseover', 'Favicon', 'age_of_domain', 'DNSRecord',
-                'web_traffic', 'Page_Rank', 'subjectivity', 'polarity', 'sentiment_label','TFIDF' 'Tweet_Label']
+                'web_traffic', 'Page_Rank', 'subjectivity', 'polarity', 'sentiment_label','TFIDF', 'Tweet_Label']
 
 feature_columns = ['Message Lenght', 'account_age', 'Follower Count','Following Count', 'User Favourite Count','no_lists', 'Retweet Count',
                 'no_hashtag', 'no_mentions','Tweet Favourite Count', 'no_digit', 'no_urls', 'having_IP_Address', 'having_At_Symbol', 
